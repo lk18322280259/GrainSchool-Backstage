90307ce0ba4b55c01d48317e590e2f036de4d94a
罗凯 <464132475@qq.com>
2022-07-09 06:46:28 +0000

    update guli_parent/service/service_vod/src/main/java/com/atguigu/vod/common/Constant.java.

--- guli_parent/service/service_vod/src/main/java/com/atguigu/vod/common/Constant.java
+++ guli_parent/service/service_vod/src/main/java/com/atguigu/vod/common/Constant.java
@@ -2,8 +2,8 @@ package com.atguigu.vod.common;
 
 public class Constant {
 
-    public static final String secretId = "AKIDX0V3SjmbGwdM1c8whMxRTXaVHqFYikeL";
-    public static final String secretKey = "c5OfwFZoHZlssjZJxhIgu7Nf2SsLuMn6";
+    public static final String secretId = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+    public static final String secretKey = "xxxxxxxxxxxxxxxxxxxxxxxxxxxx";
 
     public static final String region = "ap-guangzhou";
 }
